dc7ba108fe4f5c7116694f0ae9bbec537a02503b
sungjinw04 <abhishekpandit4224@gmail.com>
2024-08-18 09:57:27 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -30,6 +30,7 @@ UPDATE_CHAT = Config.UPDATE_CHAT
 BOT_USERNAME = Config.BOT_USERNAME 
 sudo_users = Config.sudo_users
 OWNER_ID = Config.OWNER_ID 
+OWNER_USERNAME = Config.OWNER_USERNAME
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
