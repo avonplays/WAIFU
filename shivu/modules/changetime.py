0c412614f731cc314b4b56b48e6a22ef65f4f9f0
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2024-08-20 11:57:14 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -1,21 +1,19 @@
 from pymongo import  ReturnDocument
 from pyrogram.enums import ChatMemberStatus, ChatType
-from shivu import user_totals_collection, shivuu
+from shivu import user_totals_collection, shivuu, OWNER_ID
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 ADMINS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
-
 
 @shivuu.on_message(filters.command("changetime"))
 async def change_time(client: Client, message: Message):
     
     user_id = message.from_user.id
     chat_id = message.chat.id
-    member = await shivuu.get_chat_member(chat_id,user_id)
-        
+    member = await shivuu.get_chat_member(chat_id, user_id)
 
-    if member.status not in ADMINS :
+    if member.status not in ADMINS and user_id != OWNER_ID:
         await message.reply_text('You are not an Admin.')
         return
 
@@ -26,11 +24,11 @@ async def change_time(client: Client, message: Message):
             return
 
         new_frequency = int(args[1])
-        if new_frequency < 100:
+
+        if new_frequency < 100 and user_id != OWNER_ID:
             await message.reply_text('The message frequency must be greater than or equal to 100.')
             return
 
-    
         chat_frequency = await user_totals_collection.find_one_and_update(
             {'chat_id': str(chat_id)},
             {'$set': {'message_frequency': new_frequency}},
@@ -38,6 +36,7 @@ async def change_time(client: Client, message: Message):
             return_document=ReturnDocument.AFTER
         )
 
-        await message.reply_text(f'Successfully changed {new_frequency}')
+        await message.reply_text(f'Successfully changed the message frequency to {new_frequency}.')
     except Exception as e:
-        await message.reply_text(f'Failed to change {str(e)}')
+        await message.reply_text(f'Failed to change: {str(e)}')
+
