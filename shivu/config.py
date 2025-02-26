5bf5054b258ccb604b853c72da203da2c0a5fa2a
Aditya Tripathi <abhishekpandit4224@gmail.com>
2025-02-26 17:49:16 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,18 +3,18 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = 1886390680
-    OWNER_USERNAME = "sung_jinwo4"
-    sudo_users = "1886390680, 5277261395, 5913366114, 7046072561"
+    OWNER_USERNAME = "jinwoo_sung4"
+    sudo_users = ""
     GROUP_ID = -1002216206408
-    TOKEN = "7505230606:AAEDisjieqKN4JP1rgwq0ZnXpY2jQ8sVYfU"
-    mongo_url = "mongodb+srv://tanjiro1564:tanjiro1564@cluster0.pp5yz4e.mongodb.net/?retryWrites=true&w=majority"
+    TOKEN = "7505230606:"
+    mongo_url = "mongodb+srv://:@cluster0."
     PHOTO_URL = ["https://telegra.ph/file/9d64f0c5a9c2cccbd71db.jpg"]
-    SUPPORT_CHAT = "SUNG_TALKS"
-    UPDATE_CHAT = "SUNG_NETWORKS"
-    BOT_USERNAME = "makimagrabberXrobot"
+    SUPPORT_CHAT = "eldian_network"
+    UPDATE_CHAT = "eldian_network"
+    BOT_USERNAME = "makimagrabberbot"
     CHARA_CHANNEL_ID = "-1002208868825"
-    api_id = "25064357"
-    api_hash = "cda9f1b3f9da4c0c93d1f5c23ccb19e2"
+    api_id = ""
+    api_hash = ""
 
     
 class Production(Config):
